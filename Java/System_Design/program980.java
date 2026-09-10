47b5f951e512b25bf1779e42364a9e75fe80f46d
anshumanshelke <official.anshuman.shelke@gmail.com>
2026-09-10 23:26:01 +0530

    Add files via upload

---
+++ Java/System_Design/program980.java
@@ -0,0 +1,106 @@
+// Factory type design pattern
+
+enum VehicleType
+{
+    BIKE,       // member of enum
+    CAR,
+    TRUCK
+}
+
+abstract class Vehicle
+{
+    private String number;
+
+    public Vehicle(String number)
+    {
+        this.number = number;
+    }
+
+    public String getNumber()
+    {
+        return this.number;
+    }
+
+    public abstract void display();
+
+}
+
+class Bike extends Vehicle
+{
+    public Bike(String number)
+    {
+        super(number);
+    }
+
+    public void display()
+    {
+        System.out.println("Bike :"+getNumber());
+    }
+}
+
+
+class VehicleFactory
+{
+    public static Vehicle createVehicle(VehicleType type, String number)
+    {
+        switch (type)
+        {
+            case BIKE:
+                return new Bike(number);
+
+            case CAR:
+                return new Car(number);
+
+            case TRUCK:
+                return new Truck(number); 
+
+            default:
+                throw new IllegalArgumentException("Invalid Vehicle type");
+        }
+    }
+}
+
+
+class Car extends Vehicle
+{
+    public Car(String number)
+    {
+        super(number);
+    }
+
+    public void display()
+    {
+        System.out.println("Car :"+getNumber());
+    }   
+}
+
+class Truck extends Vehicle
+{
+    public Truck(String number)
+    {
+        super(number);
+    }
+
+    public void display()
+    {
+        System.out.println("Truck :"+getNumber());
+    }
+}
+
+class program980
+{
+    public static void main(String A[])
+    {
+
+        Vehicle v1 = VehicleFactory.createVehicle(VehicleType.BIKE,"MH12VL9080");
+        Vehicle v2 = VehicleFactory.createVehicle(VehicleType.CAR,"MH12WZ9089");
+        Vehicle v3 = VehicleFactory.createVehicle(VehicleType.TRUCK,"MH12ML9080");
+        Vehicle v4 = VehicleFactory.createVehicle(VehicleType.TK,"MH12ML9080");
+    
+        v1.display();
+        v2.display();
+        v3.display();
+        v4.display();
+
+    }
+}
