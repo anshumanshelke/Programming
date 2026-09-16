7da49bfb630082c86922c20015d93bd0baea1e97
anshumanshelke <anshuman.s.shelke@gmail.com>
2026-09-16 22:28:32 +0530

    Add files via upload

--- Java/System_Design/program985.java
+++ Java/System_Design/program985.java
@@ -1,14 +1,12 @@
-/// code WITHOUT design pattern
-
 class ParkingTicket
 {
-    private int ticketNumber;
+    private  int ticketNumber;
     private String vehicleNumber;
     private int floorNumber;
     private int spotNumber;
     private String entryTime;
 
-    public ParkingTicket (int a, String b, int c, int d, String e)
+    public ParkingTicket(int a, String b, int c, int d, String e)
     {
         this.ticketNumber = a;
         this.vehicleNumber = b;
@@ -25,18 +23,17 @@ class ParkingTicket
         System.out.println("Spot Number : "+this.spotNumber);
         System.out.println("Entry Time : "+this.entryTime);
     }
-
 }
 
-public class program985
+class program985
 {
     public static void main(String A[])
     {
-        ParkingTicket p1 = new ParkingTicket(11, "MH 12 VL 7172 ", 3, 89, "9:30 AM");
-        ParkingTicket p2 = new ParkingTicket(12, "MH 14 VL 7020 ", 4, 32, "9:50 AM");
-
+        ParkingTicket p1 = new ParkingTicket(11,"MH12 VL7172",3,89,"9:30 AM");
+        ParkingTicket p2 = new ParkingTicket(12,"MH14 VL7020",4,32,"9:50 AM");
+       
         p1.display();
-        p2.display();
 
+        p2.display();
     }
 }
