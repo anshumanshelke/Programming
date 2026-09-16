eb07a02728c010b7edc06e35c1f8d621d7a02d01
anshumanshelke <anshuman.s.shelke@gmail.com>
2026-09-16 22:29:15 +0530

    Add files via upload

---
+++ Java/System_Design/program994.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+interface ParkingObserver
+{
+    void update(int availableSpots);
+}
+
+class DisplayBoard implements ParkingObserver
+{
+    public void update(int availableSpots)
+    {
+        System.out.println("Display Board : "+availableSpots);
+    }
+}
+
+class MobileApplication implements ParkingObserver
+{
+    public void update(int availableSpots)
+    {
+        System.out.println("Mobile Application : "+availableSpots);
+    }
+}
+
+class ParkingFloor
+{
+    private int availableSpots;
+
+    public ParkingFloor(int availableSpots)
+    {
+        this.availableSpots = availableSpots;
+    }
+
+}
+
+class program994
+{
+    public static void main(String A[])
+    {
+        ParkingFloor floor = new ParkingFloor(5);
+        
+    }
+}
