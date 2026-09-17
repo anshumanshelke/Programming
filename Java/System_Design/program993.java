5cc872d276bd84ceef65248079fcfdd74c12037e
anshumanshelke <anshuman.s.shelke@gmail.com>
2026-09-17 12:14:14 +0530

    Add files via upload

---
+++ Java/System_Design/program993.java
@@ -0,0 +1,33 @@
+class Demo
+{
+    public int i,j;
+
+    public Demo setI(int no)
+    {
+        this.i = no;
+        return this;
+    }
+    
+    public Demo setJ(int no)
+    {
+        this.j = no;
+        return this;
+    } 
+
+    public void display()
+    {
+        System.out.println("i : "+i);
+        System.out.println("j : "+j);
+    }
+}
+
+class program993
+{
+    public static void main(String A[])
+    {
+       new Demo()
+        .setI(11)
+        .setJ(21)
+        .display();
+    }
+}
