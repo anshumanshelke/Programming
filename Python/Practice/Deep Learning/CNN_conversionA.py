e4861028e968c1139dcd3e1342d7a8fe35f0f172
anshumanshelke <anshuman.s.shelke@gmail.com>
2026-09-24 03:02:36 +0530

    Update CNN_conversionA.py

--- Python/Practice/Deep Learning/CNN_conversionA.py
+++ Python/Practice/Deep Learning/CNN_conversionA.py
@@ -7,5 +7,9 @@ print(img.size)
 img = img.convert("L")          # L = grey scale 
 img = img.resize((2048,2048))
 
+#here change in parameters are not only for experimental purposes- 
+#But also signify that there are limits- and pixels can be concised 
+#But not Increased- to increase the quality of the image
+
 img.save("color_XX.png")
 print(img.size)
